00eb1c96dcd67a5dffb3a423c3551d7d904d9523
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-13 18:53:03 +0000

    13 Ağu 2023 21:53

--- src/CoccidiosisClassifier/config/configuration.py
+++ src/CoccidiosisClassifier/config/configuration.py
@@ -1,5 +1,4 @@
 from CoccidiosisClassifier.entity.config_entity import DataIngestionConfig
-from src.CoccidiosisClassifier.constants import *
 from src.CoccidiosisClassifier.utils.common import read_yaml, create_directories
 from src.CoccidiosisClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 
