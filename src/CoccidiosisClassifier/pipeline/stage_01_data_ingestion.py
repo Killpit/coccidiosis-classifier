6d79316737e263e9a98d84723ba6e6247eac7814
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-13 19:08:18 +0000

    13 Ağu 2023 22:08

--- src/CoccidiosisClassifier/pipeline/stage_01_data_ingestion.py
+++ src/CoccidiosisClassifier/pipeline/stage_01_data_ingestion.py
@@ -1,3 +1,16 @@
 from src.CoccidiosisClassifier.config.configuration import ConfigurationManager
 from src.CoccidiosisClassifier.components.data_ingestion import DataIngestion
 from src.CoccidiosisClassifier import logger
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config = data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
