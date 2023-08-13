35069520cd30e6a79998879da5c40dd925130de1
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-13 19:18:04 +0000

    13 Ağu 2023 22:18

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from src.CoccidiosisClassifier.pipeline.stage_01_data_ingestion import DataInges
 
 STAGE_NAME = "Data Ingestion stage"
 
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
