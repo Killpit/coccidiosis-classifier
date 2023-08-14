68a25a5ad6a31ccc4cdce7563cc18806302c9513
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-14 21:39:35 +0000

    15 Ağu 2023 00:39

--- src/CoccidiosisClassifier/config/configuration.py
+++ src/CoccidiosisClassifier/config/configuration.py
@@ -36,5 +36,12 @@ class ConfigurationManager:
         prepare_base_model_config = PrepareBaseModelConfig(
             root_dir = Path(config.root_dir),
             base_model_path = Path(config.base_model_path),
-            
+            updated_base_model_path = Path(config.updated_base_model_path),
+            params_image_size = self.params.IMAGE_SIZE,
+            params_learning_rate = self.params.LEARNING_RATE,
+            params_include_top = self.params.INCLUDE_TOP,
+            params_weights = self.params.WEIGHTS,
+            params_classes = self.params.CLASSES
         )
+
+        return prepare_base_model_config
