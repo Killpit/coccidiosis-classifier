d24269ce0be73c87bca713d9cd9cb38fb0a1367c
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-14 21:00:50 +0000

    15 Ağu 2023 00:00

--- src/CoccidiosisClassifier/pipeline/stage_01_data_ingestion.py
+++ src/CoccidiosisClassifier/pipeline/stage_01_data_ingestion.py
@@ -11,7 +11,7 @@ class DataIngestionTrainingPipeline:
     def main(self):
         config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config = data_ingestion_config)
+        data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
 
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
         obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx===========x")
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
