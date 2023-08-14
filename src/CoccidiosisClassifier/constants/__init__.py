bef52bbf8fc349d858f381eebbdb3411e198a8c3
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-14 21:13:08 +0000

    15 Ağu 2023 00:13

--- src/CoccidiosisClassifier/constants/__init__.py
+++ src/CoccidiosisClassifier/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("config/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
+CONFIG_FILE_PATH = Path("/Users/atatekeli/PycharmProjects/coccidiosis-classifier/config/config.yaml")
+PARAMS_FILE_PATH = Path("/Users/atatekeli/PycharmProjects/coccidiosis-classifier/config/config.yaml")
