8d484f3fff95333f98c3f9b1c6449596c0c4b1bf
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-16 11:03:01 +0000

    16 Ağu 2023 14:03

--- main.py
+++ main.py
@@ -3,23 +3,22 @@ from src.CoccidiosisClassifier.pipeline.stage_01_data_ingestion import DataInges
 from src.CoccidiosisClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
 
 STAGE_NAME = "Prepare base model"
-
-try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    data_ingestion = PrepareBaseModelTrainingPipeline()
-    data_ingestion.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
