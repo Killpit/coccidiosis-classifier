91270d2ced7532cdee5f0c97a9276d8286d77966
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-19 18:44:09 +0000

    19 Ağu 2023 21:44

--- src/CoccidiosisClassifier/pipeline/stage_03_training.py
+++ src/CoccidiosisClassifier/pipeline/stage_03_training.py
@@ -2,3 +2,22 @@ from src.CoccidiosisClassifier.config.configuration import ConfigurationManager
 from src.CoccidiosisClassifier.components.prepare_callbacks import PrepareCallback
 from src.CoccidiosisClassifier.components.training import Training
 
+STAGE_NAME = "Training"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_callbacks_config = config.get_prepare_callback_config()
+        prepare_callbacks = PrepareCallback(config = prepare_callbacks_config)
+        callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
+
+        training_config = config.get_training_config()
+        training = Training(config = training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train(
+            callback_list = callback_list
+        )
