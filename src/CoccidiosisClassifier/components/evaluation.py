110f8e10084f130d08d59440e5b56dcbb454fb6b
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-21 21:22:47 +0000

    22 Ağu 2023 00:22

---
+++ src/CoccidiosisClassifier/components/evaluation.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+from pathlib import Path
+from src.CoccidiosisClassifier.entity.config_entity import EvaluationConfig
+from src.CoccidiosisClassifier.utils.common import save_json
+
