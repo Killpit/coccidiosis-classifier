9cd3b0ae2d24ebc26914a28b010d41f1508cf55d
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-25 16:52:14 +0300

    Final step before AWS and Azure deployment, web app created

--- app.py
+++ app.py
@@ -15,8 +15,25 @@ class ClientApp:
         self.filename = "inputImage.jpg"
         self.classifier = PredictionPipeline(self.filename)
 
-@app.route("/", methods = ['GET'])
+@app.route("/", methods=['GET'])
 @cross_origin()
 def home():
     return render_template('index.html')
 
+@app.route("/train", methods=['GET','POST'])
+@cross_origin()
+def trainRoute():
+    os.system("python main.py")
+    return "Training done successfully!"
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRoute():
+    image = request.json['image']
+    decodeImage(image, clApp.filename)
+    result = clApp.classifier.predict()
+    return jsonify(result)
+
+if __name__ == "__main__":
+    clApp = ClientApp()
+    app.run(host='0.0.0.0', port=8080) #local host
